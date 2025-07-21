027eac6aa6177193dd0d6553b9611d91a7417fc3
Vector99dev <challengemode45@gmail.com>
2025-07-21 18:48:25 +0900

    fix: env

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,11 @@ const nextConfig: NextConfig = {
     ],
     domains: ['img-v2.gtsstatic.net', 'm.sothebysrealty.com', 'sothebysrealty.com'],
   },
+  env: {
+    NEXT_PUBLIC_LIVEKIT_URL: process.env.NEXT_PUBLIC_LIVEKIT_URL,
+    LIVEKIT_API_KEY: process.env.LIVEKIT_API_KEY,
+    LIVEKIT_API_SECRET: process.env.LIVEKIT_API_SECRET,
+  },
 };
 
 export default nextConfig;
